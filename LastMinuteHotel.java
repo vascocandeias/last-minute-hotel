e5cdf992066c4554fbf5acf28a86ec338aabd952
vcandeias <42552118+vcandeias@users.noreply.github.com>
2019-04-09 20:10:29 +0800

    welcome page

--- LastMinuteHotel.java
+++ LastMinuteHotel.java
@@ -1,11 +1,38 @@
+import java.util.*;
 import root.bookings.*;
 import root.houses.*;
 import root.users.*;
 
 public class LastMinuteHotel {
+
+  private static final int NUM_USERS = 10;
+
   public static void main(String[] args) {
-    System.out.println("Hey");
-    House house = new House();
-    Booking booking = new Booking();
+
+    Scanner kb = new Scanner(System.in);
+
+    System.out.println("Welcome to Last Minute Hotel Booking!");
+    System.out.println("Choose an action:");
+    System.out.println("l - login");
+    System.out.println("r - register");
+    System.out.println("q - quit");
+
+    menu:
+    while(true){
+      try {
+        switch(kb.nextLine().charAt(0)){
+          case 'l':
+            System.out.println("login!");
+            break;
+          case 'r':
+            System.out.println("register!");
+            break;
+          case 'q':
+            break menu;
+        }
+      } catch(StringIndexOutOfBoundsException e) {
+        continue;
+      }
+    }
   }
 }
